3023a81801c85029b3d9254b8e16d0a885abadb5
laierr <kenderberator@gmail.com>
2014-04-28 23:59:21 +0300

    Profiles

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+  , _ = require('underscore')
+  , app = express();
 
 app.get('/', function(req, res){
   res.send('Check your privilege!');
+});
+
+// /p/username
+// {result: "Username is privileged as hell!"}
+
+app.get('/p/:username', function(req, res){
+  res.send({result: req.param('username') + ' is privileged as hell!'});
 });
 
 var server = app.listen(3000, function() {
